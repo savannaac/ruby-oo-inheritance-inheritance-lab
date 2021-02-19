874c1a6a3d71cff4d268d5195cec41027ed33ed7
savanna chiang <savannaac@gmail.com>
2021-02-18 20:59:23 -0600

    OO Inheritance lab - complete

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+    
+    def initialize
+        @knowledge = []
+    end
 
+    def learn(knowlege_string)
+        @knowledge << knowlege_string
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
